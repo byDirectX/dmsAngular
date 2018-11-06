9f8988c891730dff56cae679154ca2e820ea6929
byDirectX <gdirectx@gmail.com>
2018-11-07 02:21:44 +0300

    Fix

--- server.js
+++ server.js
@@ -7,14 +7,10 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/dmsAngular'));
 
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist/dmsAngular/index.html'));
-  app.use(express.static('./dist/dmsAngular'));
+app.get('/*', function(req,res) {
 
-  app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, '/dist/dmsAngular/index.html'));
-  });
+  res.sendFile(path.join(__dirname+'/dist/dmsAngular/index.html'));
+});
 
 // Start the app by listening on the default Heroku port
-  app.listen(process.env.PORT || 8080)
-});
+app.listen(process.env.PORT || 8080);
